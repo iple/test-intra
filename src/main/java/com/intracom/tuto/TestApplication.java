1c7f88ee42fd27db92f175e955157af6784a832b
George Tsaloukidis <georgetsalouk@yahoo.gr>
2017-11-10 10:32:47 +0000

    Resteasy changes

---
+++ src/main/java/com/intracom/tuto/TestApplication.java
@@ -0,0 +1,25 @@
+package com.intracom.tuto;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.core.Application;
+
+public class TestApplication extends Application {
+   private Set<Object> singletons = new HashSet<Object>();
+   private Set<Class<?>> empty = new HashSet<Class<?>>();
+
+   public TestApplication() {
+      singletons.add(new SampleResource());
+   }
+
+   @Override
+   public Set<Class<?>> getClasses() {
+      return empty;
+   }
+
+   @Override
+   public Set<Object> getSingletons() {
+      return singletons;
+   }
+}

4ef7874a8ae1ac61288baaf4f5c307dafffccc60
George Tsaloukidis <gtsal@intracom-telecom.com>
2017-11-23 11:01:35 +0200

    Maps

--- src/main/java/com/intracom/tuto/SampleResource.java
+++ src/main/java/com/intracom/tuto/SampleResource.java
@@ -11,13 +11,12 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 @Path("/main")
 public class SampleResource {
     @GET
-    @Path("/greedings")
+    @Path("/elements")
     @Produces(MediaType.APPLICATION_JSON)
     public String getGreedings() {
         ObjectMapper mapper = new ObjectMapper();
         ObjectNode rootObj = mapper.createObjectNode();
-        rootObj.put("message", "Hello George");
+        ArrayNode elementsArray = rootObj.addArray();
         return rootObj.toString();
     }
-    
 }
